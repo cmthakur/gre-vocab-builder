3080fd04be8dfc122de0a7ad06e8f51df7b0d773
Dipil <dipil.saud@gmail.com>
2012-10-27 23:25:02 +0545

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GreWords::Application.config.secret_token = '311d38df9cf4d91cb32f5f96edf436b4ae3064abe65b89845e759bfc156de0fae31a3b609e3dd6acd3207be8a7f03329933ba40e7fcd212db00397ef1b210196'
